ef4f024e6cd85fa2657e48a888a58de35dddaab4
Joaquim Verges <joaquim.verges@gmail.com>
2022-04-28 13:40:19 -0700

    mintable azuki

--- contracts/AzukiWithMinting.sol
+++ contracts/AzukiWithMinting.sol
@@ -8,8 +8,18 @@ import '@thirdweb-dev/contracts/feature/interface/IMintableERC721.sol';
 contract AzukiWithMinting is ERC721A, ThirdwebContract, IMintableERC721 {
     constructor(string memory name, string memory symbol) ERC721A(name, symbol) {}
 
-    function mintTo(address to, string calldata uri) public override returns (uint256);
-        // _safeMint's second argument now takes in a quantity, not a tokenId.
-        _safeMint(msg.sender, quantity);
+    mapping(uint256 => string) private uris;
+
+    /// Adapted Azuki mint function to accept a URI, and work in our dashboard
+    function mintTo(address to, string calldata uri) external returns (uint256) {
+        uint256 id = _currentIndex;
+        _safeMint(to, 1);
+        uris[id] = uri;
+        emit TokensMinted(to, id, uri);
+        return id;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return uris[tokenId];
     }
 }
