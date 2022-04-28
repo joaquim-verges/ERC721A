8095989e60609a144f671f4d2c96fe07679a6ebb
Joaquim Verges <joaquim.verges@gmail.com>
2022-04-28 13:55:17 -0700

    added comments

--- contracts/AzukiSimple.sol
+++ contracts/AzukiSimple.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.4;
 import './ERC721A.sol';
 import '@thirdweb-dev/contracts/ThirdwebContract.sol';
 
+/// @title Pure ERC721A contract that can be used with the thirdweb SDK
 contract SimpleAzuki is ERC721A, ThirdwebContract {
     constructor(string memory name, string memory symbol) ERC721A(name, symbol) {}
 
     function mint(uint256 quantity) external payable {
-        // _safeMint's second argument now takes in a quantity, not a tokenId.
         _safeMint(msg.sender, quantity);
     }
 }
--- contracts/AzukiWithMinting.sol
+++ contracts/AzukiWithMinting.sol
@@ -5,12 +5,13 @@ import './ERC721A.sol';
 import '@thirdweb-dev/contracts/ThirdwebContract.sol';
 import '@thirdweb-dev/contracts/feature/interface/IMintableERC721.sol';
 
+/// @title Azuki contract that can be fully used in the thirdweb dashboard
 contract AzukiWithMinting is ERC721A, ThirdwebContract, IMintableERC721 {
     constructor(string memory name, string memory symbol) ERC721A(name, symbol) {}
 
     mapping(uint256 => string) private uris;
 
-    /// Adapted Azuki mint function to accept a URI, and work in our dashboard
+    /// Modified Azuki mint function to accept a URI, and work in our dashboard
     function mintTo(address to, string calldata uri) external returns (uint256) {
         uint256 id = _currentIndex;
         _safeMint(to, 1);
@@ -19,6 +20,7 @@ contract AzukiWithMinting is ERC721A, ThirdwebContract, IMintableERC721 {
         return id;
     }
 
+    /// Modified tokenURI to return more than just base URI
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return uris[tokenId];
     }
